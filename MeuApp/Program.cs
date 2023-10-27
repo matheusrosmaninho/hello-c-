016d3a72b570101b709662d3449e8a1f8b7a18fb
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-10-27 07:17:50 -0300

    Initial commit

---
+++ MeuApp/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product();
+            product.Id = 1;
+            product.Title = "teste";
+            product.Price = 10;
+            Console.WriteLine(product.PriceInDolar(5));
+        }
+
+    }
+
+    struct Product
+    {
+        public int Id;
+        public float Price;
+
+        public string Title;
+
+        public Product(int id, string title, float price)
+        {
+            Id = id;
+            Title = title;
+            Price = price;
+        }
+
+        public float PriceInDolar(float dolar)
+        {
+            return Price * dolar;
+        }
+    }
+}
