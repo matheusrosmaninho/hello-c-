b4522f6188244c7bfec7065af6de702e76b2a76a
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-10-28 18:40:06 -0300

    feat: Creating a text editor

---
+++ TextEditor/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace TextEditor;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Menu();
+    }
+
+    public static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("O que deseja fazer?");
+        Console.WriteLine("1 - Abrir arquivo");
+        Console.WriteLine("2 - Criar novo arquivo");
+        Console.WriteLine("0 - Sair");
+
+        short option = short.Parse(Console.ReadLine());
+
+        switch (option)
+        {
+            case 0:
+                Environment.Exit(0);
+                break;
+            case 1:
+                Abrir();
+                break;
+            case 2:
+                Editar();
+                break;
+            default:
+                Environment.Exit(0);
+                break;
+        }
+    }
+
+    public static void Abrir() { }
+
+    public static void Editar()
+    {
+        Console.Clear();
+        Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+        Console.WriteLine("--------------------------------------");
+        string text = "";
+
+        do
+        {
+            text += Console.ReadLine();
+            text += Environment.NewLine;
+        }
+        while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+        Console.Write(text);
+    }
+}
