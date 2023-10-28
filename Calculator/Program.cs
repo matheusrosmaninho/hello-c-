70ac528bf772a82ff6599badcdc23b83707e6c23
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-10-28 08:22:29 -0300

    Create sum betewen 2 numbers

---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Calculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o primeiro valor:");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo valor");
+        float v2 = float.Parse(Console.ReadLine());
+
+        float resultado = v1 + v2;
+        Console.WriteLine("O resultado é " + resultado);
+        Console.WriteLine($"O resultado é: {resultado}");
+    }
+}
