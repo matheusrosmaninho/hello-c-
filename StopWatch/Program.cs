671799bdcb0c3689cb8a20facb7830e352e5bfe8
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-10-28 16:10:20 -0300

    fix: Get parts of string

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,9 +1,44 @@
 ﻿namespace StopWatch;
 
+using System.Threading;
+
 class Program
 {
     public static void Main(string[] args)
     {
+        Menu();
+    }
 
+    public static void Menu() {
+        Console.Clear();
+        Console.WriteLine("S = segundos => 10s = 10 segundos");
+        Console.WriteLine("M = minuto => 1m = 1 minuto");
+        Console.WriteLine("0 = Sair");
+        Console.WriteLine("Quanto tempo deseja rodar a aplicação?");
+
+        string data = Console.ReadLine().ToLower();
+        char type = char.Parse(data.Substring(data.Length - 1, 1));
+        int time = int.Parse(data.Substring(0, data.Length - 1));
+
+        Console.WriteLine(data);
+        Console.WriteLine(time);
+        Console.WriteLine(type);
+    }
+
+    public static void Start(int time)
+    {
+        int cuurentTime = 0;
+
+        while(cuurentTime != time)
+        {
+            Console.Clear();
+            cuurentTime++;
+            Console.WriteLine(cuurentTime);
+            Thread.Sleep(1000);
+        }
+
+        Console.Clear();
+        Console.WriteLine("Stop watch finalizado ...");
+        Thread.Sleep(2500);
     }
 }
