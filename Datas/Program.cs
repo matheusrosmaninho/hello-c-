41b515305649276ac82edc59347e671bd0233e5e
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-10-30 23:33:38 -0300

    learn about dates

---
+++ Datas/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Datas;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+
+        var data = new DateTime();
+        Console.WriteLine(data);
+    }
+}
