d54a6213b49fd85b5f2f57d71aa81db1db5a414b
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-11-21 17:07:54 -0300

    fix: Learn about date

--- Datas/Program.cs
+++ Datas/Program.cs
@@ -8,5 +8,25 @@ public class Program
 
         var data = new DateTime();
         Console.WriteLine(data);
+
+        var datetimeUtc = DateTime.UtcNow;
+        Console.WriteLine("Data utc0: " + datetimeUtc);
+
+        Console.WriteLine("Hora local: " + datetimeUtc.ToLocalTime());
+
+        var timezoneAustralia = TimeZoneInfo.FindSystemTimeZoneById("Pacific/Auckland");
+        Console.WriteLine(timezoneAustralia);
+
+        foreach( var timezone in TimeZoneInfo.GetSystemTimeZones()) {
+            Console.WriteLine(timezone.Id);
+            Console.WriteLine(timezone);
+        }
+
+        Console.Clear();
+        Console.WriteLine(IsWeekend(DateTime.Now.DayOfWeek));
+    }
+
+    static bool IsWeekend(DayOfWeek today) {
+        return today == DayOfWeek.Saturday || today == DayOfWeek.Sunday;
     }
 }
