3bbaad0f1538ee66fec988fe2a2dde0bec9c6856
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-11-22 15:46:19 -0300

    fix: Moedas

---
+++ Moedas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Moedas;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        decimal valor = 10536.25M;
+        Console.WriteLine(valor.ToString(
+            "C",
+            CultureInfo.CreateSpecificCulture("pt-BR"))
+        );
+
+        Console.Clear();
+
+        Console.WriteLine(
+            Math.Floor(valor)
+        );
+    }
+}
