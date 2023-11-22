43494585d2e7d5a088556d93098a9ed76c609bc8
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-11-22 15:46:03 -0300

    feat: List

---
+++ Lista/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lista;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var funcionarios = new Funcionario[5];
+        funcionarios[0] = new Funcionario() {Id = 2579, Nome = "André"};
+
+        foreach(var funcionario in funcionarios) {
+            Console.WriteLine(funcionario.Id);
+            Console.WriteLine(funcionario.Nome);
+        }
+    }
+}
+
+public struct Funcionario {
+    public int Id { get; set; }
+
+    public string Nome {get; set;}
+}
